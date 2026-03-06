97c16da6f79b44e7fb38a4f3aea290c95d9c1a7f
Manas Kushwaha <manaskushwaha123@gmail.com>
2026-03-06 16:44:42 +0530

    starting

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white h-screen flex items-center justify-center">
+      Tailwind + React Router working
+    </div>
+  );
+}
+
+export default App;
