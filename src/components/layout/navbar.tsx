5c8085db5c4411525faa85c5b5351acde925e794
Manas Kushwaha <manaskushwaha123@gmail.com>
2026-03-08 13:29:05 +0530

    fixednavbar a lil

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <div className="p-5">
+    <div className="p-3.5">
       <nav className="flex items-center justify-between px-9 py-2 text-gray-600 glass-card">
         <div className="flex gap-9 text-sm tracking-wide">
           <Link
